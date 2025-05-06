af615e39f4da225c0a7e9dac81a7e920c1b305ed
Ítalo Polazzi <itpzzi@outlook.com>
2025-05-06 17:02:42 -0300

    refactor(category): use fixtures in API tests and ensure assertion with consistent ordering

--- src/django_project/category_app/tests/test_category_api.py
+++ src/django_project/category_app/tests/test_category_api.py
@@ -1,26 +1,43 @@
-from django.test import TestCase
-from rest_framework.test import APITestCase
+import pytest
+from rest_framework.test import APIClient
 
+from operator import itemgetter
 from django_project.category_app.repository import DjangoORMCategoryRepository
 from src.core.category.domain.category import Category
 
 
-class TestCategoryAPI(APITestCase):
-    def test_list_categories(self):
+@pytest.mark.django_db
+class TestCategoryAPI:
 
-        category_movie = Category(name="Filme", description="Longas divertidos")
-        category_series = Category(name="Séries", description="Curtas divertidas")
-        category_documentary = Category(
-            name="Documentários", description="Curtas informativas"
-        )
+    @pytest.fixture
+    def category_movie(self) -> Category:
+        return Category(name="Filme", description="Longas divertidos")
 
-        repository = DjangoORMCategoryRepository()
-        repository.save(category_movie)
-        repository.save(category_series)
-        repository.save(category_documentary)
+    @pytest.fixture
+    def category_series(self) -> Category:
+        return Category(name="Séries", description="Curtas divertidas")
+
+    @pytest.fixture
+    def category_documentary(self) -> Category:
+        return Category(name="Documentários", description="Curtas informativas")
+
+    @pytest.fixture
+    def category_repository(self) -> DjangoORMCategoryRepository:
+        return DjangoORMCategoryRepository()
+
+    def test_list_categories(
+        self,
+        category_movie: Category,
+        category_series: Category,
+        category_documentary: Category,
+        category_repository: DjangoORMCategoryRepository,
+    ) -> None:
+        category_repository.save(category_movie)
+        category_repository.save(category_series)
+        category_repository.save(category_documentary)
 
         url = "/api/categories/"
-        response = self.client.get(url)
+        response = APIClient().get(url)
 
         expected_data = [
             {
@@ -30,19 +47,21 @@ class TestCategoryAPI(APITestCase):
                 "is_active": category_movie.is_active,
             },
             {
-                "id": str(category_series.id),
-                "name": category_series.name,
-                "description": category_series.description,
-                "is_active": category_series.is_active,
-            },
-            {
                 "id": str(category_documentary.id),
                 "name": category_documentary.name,
                 "description": category_documentary.description,
                 "is_active": category_documentary.is_active,
             },
+            {
+                "id": str(category_series.id),
+                "name": category_series.name,
+                "description": category_series.description,
+                "is_active": category_series.is_active,
+            },
         ]
 
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 3)
-        self.assertCountEqual(response.data, expected_data)
+        assert response.status_code == 200
+        assert len(response.data) == 3
+        assert sorted(response.data, key=itemgetter("name")) == sorted(
+            expected_data, key=itemgetter("name")
+        )
