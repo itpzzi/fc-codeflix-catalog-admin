4d44e260ad55091830891f990c8b4c675b6d92d3
Ítalo Polazzi <itpzzi@outlook.com>
2025-05-20 00:01:33 -0300

    feat(video): add admin for video app Video and media models

--- src/django_project/video_app/admin.py
+++ src/django_project/video_app/admin.py
@@ -1,1 +1,21 @@
 # Register your models here.
+from django.contrib import admin
+
+from src.django_project.video_app.models import (
+    AudioVideoMedia as AudioVideoMediaModel,
+)
+from src.django_project.video_app.models import (
+    Video as VideoModel,
+)
+
+
+class VideoAdmin(admin.ModelAdmin):
+    pass
+
+
+class AudioVideoMediaAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(VideoModel, VideoAdmin)
+admin.site.register(AudioVideoMediaModel, AudioVideoMediaAdmin)
